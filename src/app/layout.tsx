a2b3b6c1702636c3fd864b8a0ff64ba14252f6a8
brodwolf <wilham@brodwolf.dev>
2024-05-18 03:43:28 -0300

    feat: add vercel-speed-insights provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 import "./globals.css";
@@ -28,6 +29,7 @@ export default function RootLayout({
       </head>
       <body>{children}</body>
       <Analytics />
+      <SpeedInsights />
     </html>
   );
 }
